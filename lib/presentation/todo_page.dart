4c482075c86d1637c533af8af83d4e3c667f22c1
adwitafernanda <adwitafernanda4@gmail.com>
2025-05-08 22:35:56 +0700

    ADD - aksi untuk menambahkan Todo dengan validasi dan tanggal terpilih

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -71,6 +71,21 @@ class TodoPage extends StatelessWidget{
                         },
                       ),
                     ),
+                    FilledButton(
+                      onPressed: () {
+                        if (_key.currentState!.validate()) {
+                          final selectedDate = context.read<TodoBloc>().state;
+                          if (selectedDate is TodoLoaded) {
+                            context.read<TodoBloc>().add(
+                              TodoEventAdd(
+                                title: _controller.text,
+                                date: selectedDate.selectedDate,
+                              ),
+                            );
+                          }
+                        }
+                      }
+                    )
                   ],
                 ),
               ),
