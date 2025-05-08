10bf70a0271805de3aeb936f5671f86054dc9e01
adwitafernanda <adwitafernanda4@gmail.com>
2025-05-08 22:21:55 +0700

    ADD - struktur awal build TodoPage

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -2,4 +2,11 @@ import 'package:flutter/material.dart';
 
 class TodoPage extends StatelessWidget{
   const TodoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final _key = GlobalKey<FormState>();
+    final _controller = TextEditingController();
+    return ;
+  }
 }
