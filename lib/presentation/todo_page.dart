fcce717620a9d7fec912483c67d18732828d9aef
adwitafernanda <adwitafernanda4@gmail.com>
2025-05-08 22:38:10 +0700

    ADD - teks 'Tambah' pada tombol

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -86,6 +86,7 @@ class TodoPage extends StatelessWidget{
                           }
                         }
                       },
+                      child: Text('Tambah'),
                     ),
                   ],
                 ),
